ad06149689bcd09a46271e5b51101af33ae44b92
Joris <Neyens01@gmail.com>
2021-09-06 15:23:35 +0200

    add button and logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import "../styles/styles.scss";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Head from 'next/head'
 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://use.typekit.net/myt1pll.css"/>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
